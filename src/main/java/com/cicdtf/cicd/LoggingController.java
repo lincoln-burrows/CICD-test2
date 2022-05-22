dea0c9e1292c7671cab78498991d7a71fde094b1
lincoln-burrows <minseong.kim7000@gmail.com>
2022-05-23 00:17:22 +0900

    Initial commit

---
+++ src/main/java/com/cicdtf/cicd/LoggingController.java
@@ -0,0 +1,23 @@
+package com.cicdtf.cicd;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@PropertySource("classpath:application.properties")
+public class LoggingController {
+
+    @Value("${logging-module.version}")
+    private String version;
+
+    @GetMapping("/")
+    public String version() {
+        return String.format("Project Version : %s", version);
+    }
+
+    @GetMapping("/health") public String checkHealth() {
+        return "healthy";
+    }
+}
