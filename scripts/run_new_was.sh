883686b3233de0400ce7cd57acce7c87b4710864
lincoln-burrows <minseong.kim7000@gmail.com>
2022-05-24 14:21:33 +0900

    appspec.yml

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -21,7 +21,8 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/playground_logging/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/Chorok/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
+#nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/playground_logging/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
 #nohup java -jar -Dserver.port=${TARGET_PORT} playground-logging/build/libs/cicd-0.0.1-SNAPSHOT.jar > /home/ubuntu/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
